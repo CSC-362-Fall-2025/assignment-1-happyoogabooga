3dc65847b4bd0b408d2824d3ab40d8d6299e2051
Jerry_Moyer <brubru0724@gmail.com>
2026-01-20 11:07:53 -0500

    Made the entire program, including user input for elements and dynamic memory allocation

--- question1.c
+++ question1.c
@@ -1,8 +1,30 @@
-//Place your needed include statements here (This is roughly the equivelent of libraries in python)
-
+//Jerry Moyer this is my own work
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
-  //Place your solution code here
+  //initialize the amount at 0
+  int amount = 0;
 
+  while(amount <= 0){
+    printf("enter a positive whole number for the amount your array will hold\n");
+    scanf("%d", &amount);
+  }
+
+  int * arr = (int*)malloc(sizeof(int) * amount);
+  for(int i = 0; i < amount; i++){
+    int element = 0;
+    printf("\nenter some numbers for this array:");
+    scanf("%d", &element);
+
+    //the value at the address of our starting index plus the
+    //index of the element is the element
+    *(arr + i * sizeof(int)) = element;
+  }
+  for(int i = amount -1; i >= 0; i--){
+    printf("\n%d", *(arr + sizeof(int) * i));
+  }
+  //free our array
+  free(arr);
   return 0;
 }
